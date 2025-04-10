c6c2bc33e21ea4279abdc841c8527d9ccc7b9e01
gts5045gts <gts5045gts@192.168.219.106>
2025-04-11 00:08:27 +0900

    MemberService create

--- springGit/src/main/java/com/itwillbs/controller/MemberController.java
+++ springGit/src/main/java/com/itwillbs/controller/MemberController.java
@@ -1,8 +1,21 @@
 package com.itwillbs.controller;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
+
+import com.itwillbs.service.MemberService;
 
 @Controller
 public class MemberController {
+	
+	@Inject
+	MemberService memberService;
+	
+	
+	
+	
+	
+	
 
 }
